e8fdcfc4e54015492b46f8b49d48a53d87391759
WallysonMarcos <wallyson78@gmail.com>
2019-04-11 20:18:30 -0400

    Update listalinear.c

---
+++ lista/arranjos/listalinear.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct tList
+{
+	int aValues[255];
+	int nLastPos;
+
+}oList;
+
+
+void SetLastPos(oList *pList, int value )
+{
+	pList->nLastPos += value;
+}
+
+void InsertFim(oList *pList, int nvalue)
+{	
+	SetLastPos(pList,1);
+	pList->aValues[pList->nLastPos] = nvalue;		
+}
+
+void InsetInto(oList *pList,int pos, int value)
+{
+	int lSetArray = 1;
+	//Valida se a possição é valida
+	if (pos > pList->nLastPos || pos < 0)
+		lSetArray = 0;
+
+	if (lSetArray == 1)
+	{
+		printf("Inserindo Meio \n");
+		//Incrementa +1 pos no array
+		SetLastPos(&pList,1);
+		//Realoca o array até a posição desejada
+		for (int i = pList->nLastPos ; i > pos; i--)
+			pList->aValues[i] = pList->aValues[i - 1];
+		//seta o novo valor para posição desejada
+		pList->aValues[pos] = value;				
+	}
+	else
+	{
+		printf("Inserindo Fim \n");
+		InsertFim(&pList,value);
+	}
+}
+
+void Remove(oList *pList,int pos)
+{
+	int lSetArray = 1;
+	//Valida se a possição é valida
+	if (pos > pList->nLastPos || pos < 0)
+		lSetArray = 0;
+
+	if (lSetArray == 1)
+	{
+		for (int i = 0; i <= pList->nLastPos ; i++)
+		{
+			if (i < pos)
+				continue;
+
+			pList->aValues[i] = pList->aValues[i + 1];
+		}
+		SetLastPos(&pList,-1);
+	}
+	else
+	{
+		printf("Imposivel remover na pos %d \n", pos);
+	}
+}
+
+void PrintList(oList pList)
+{
+	for (int i = 0; i <= pList.nLastPos; i++)
+	{
+		printf("Pos: %d Valor: %d \n", i, pList.aValues[i]);
+	}
+}
+
+
+int main()
+{
+	oList pList;
+	pList.nLastPos = -1;
+
+	printf("Tamanho inicial: %d \n", pList.nLastPos);
+	InsertFim(&pList,10);
+    InsertFim(&pList,20);
+	InsertFim(&pList,40);	
+	PrintList(pList);
+	printf("Tamanho Atual: %d \n", pList.nLastPos);
+	InsetInto(&pList,2,30);
+	InsetInto(&pList,5,60);
+	InsetInto(&pList,4,50);
+	PrintList(pList);
+
+	printf("Tamanho Atual: %d \n", pList.nLastPos);
+	Remove(&pList,3);
+	PrintList(pList);
+	//system("pause");
+	return 0;
+}
